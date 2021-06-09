2be9f89e96d0359b9f9be3549082c234effa2f56
JeromeRReduta <38510370+JeromeRReduta@users.noreply.github.com>
2021-06-09 13:15:06 -0700

    Finished basic InvertedIndex implementation

--- src/main/java/InvertedIndex.java
+++ src/main/java/InvertedIndex.java
@@ -1,3 +1,5 @@
+import java.io.IOException;
+import java.nio.file.Path;
 import java.util.Collections;
 import java.util.Set;
 import java.util.TreeMap;
@@ -16,7 +18,6 @@ public class InvertedIndex {
 	 */
 	private final TreeMap<String, TreeMap<String, TreeSet<Integer>>> map;
 	
-	
 	/**
 	 * Constructor
 	 */
@@ -31,22 +32,54 @@ public class InvertedIndex {
 	}
 	
 	public Set<String> get() {
-		return map != null ? Collections.unmodifiableSet( map.keySet() ) :
-			Collections.emptySet();
+		return Collections.unmodifiableSet( map.keySet() );
 	}
 	
 	public Set<String> get(String stem) {
-		boolean outerAndInnerMapExist = map != null && map.get(stem) != null;
-		
-		return outerAndInnerMapExist ? Collections.unmodifiableSet( map.get(stem).keySet() ) :
-			Collections.emptySet();
+		return map.get(stem) != null ?
+				Collections.unmodifiableSet( map.get(stem).keySet() ) : Collections.emptySet();
 	}
 	
 	public Set<Integer> get(String stem, String pathName) {
-		boolean outerAndInnerMapExist = map != null && map.get(stem) != null;
-		
-		return outerAndInnerMapExist ? Collections.unmodifiableSet( map.get(stem).get(pathName) ) :
-			Collections.emptySet();
+		return map.get(stem) != null && map.get(stem).get(pathName) != null
+				? Collections.unmodifiableSet( map.get(stem).get(pathName) ) : Collections.emptySet();
 	}
+	
+	public boolean contains(String stem) {
+		return map.containsKey(stem);
+	}
+	
+	public boolean contains(String stem, String pathName) {
+		return contains(stem) && map.get(stem) != null && map.get(stem).containsKey(pathName);
+	}
+	
+	public boolean contains(String stem, String pathName, int position) {
+		return contains(stem, pathName) && map.get(stem).get(pathName) != null &&
+				map.get(stem).get(pathName).contains(position);
+	}
+	
+	public int size() {
+		return map.size();
+	}
+	
+	public int innerMapSize(String stem) {
+		return contains(stem) ? map.get(stem).size() : 0;
+	}
+	
+	public int locationListSize(String stem, String pathName) {
+		return contains(stem, pathName) ? map.get(stem).get(pathName).size() : 0;
+	}
+	
+	@Override
+	public String toString() {
+		return map.toString();
+	}
+	
+	public void toFile(final Path path) throws IOException {
+		//TODO: add toFile
+	}
+	
+	//TODO: Search logic comes later
+	
 	
 }
